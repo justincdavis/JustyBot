611b54bf5b8fa6a26421344f651ec9c0543ef674
justincdavis <davis.justin@mssm.org>
2021-10-03 22:34:33 -0600

    echo, join, skip, stop, play_next_song, get_next_song complete

--- cogs/music.py
+++ cogs/music.py
@@ -1,50 +1,59 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands.bot import when_mentioned_or
-from classes.song_queue import Song,Song_Queue
+from classes.song_queue import Song,Song_Queue,get_song_youtube
+import sys
+
+#GLOBAL OPTIONS
+FFMPEG_OPTIONS = {'before_options': '-reconnect 1', 'options': '-vn'}
 
 #bot class to handle all commands
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.song_queue = Song_Queue([])
-        self.FFMPEG_OPTIONS = {'before_options': '-reconnect 1', 'options': '-vn'}
+
     def get_next_song(self):
-        if(len(self.song_queue) > 0):
-            return self.song_queue[0].get_title()
+        return self.song_queue.get_next_song()
+
+    async def play_next_song(self, ctx):
+        song = self.get_next_song()
+        if(song != None):
+            url = self.get_next_song()
+        else:
+            raise commands.CommandError("No songs in the queue")
+        source = discord.FFmpegPCMAudio(source=url, **FFMPEG_OPTIONS, stderr=sys.stdout)
+        ctx.voice_client.play(source, after=lambda e: print(f'Player error: {e}') if e else None)
+
+    @commands.command
+    async def echo(self, ctx):
+        await ctx.send(ctx.message.content[5::])
 
     @commands.command()
     async def join(self, ctx):
         if ctx.author.voice == None or ctx.author.voice.channel == None:
-            return        
+            raise commands.CommandError("User is not in a voice channel")      
         channel = ctx.message.author.voice.channel
         await channel.connect()
 
     @commands.command()
     async def leave(self, ctx):
-        if ctx.voice_client:
+        if ctx.guild.voice_client:
             await ctx.guild.voice_client.disconnect()
         else:
-            return
+            raise commands.CommandError("Not connected to a voice channel")  
 
     @commands.command()
     async def stop(self, ctx):
         self.leave(ctx)
 
-# #display a message to console when the bot connects
-# @bot.event
-# async def on_ready():
-#     print("Logged on as {0}!".format(bot.user))
-
-# @bot.event
-# async def on_command_error(ctx, error):
-#     if isinstance(error, discord.ext.commands.errors.CommandNotFound):
-#         await ctx.send("Command not found")
-
-# #simple echo test command
-# @bot.command()
-# async def echo(ctx):
-#     await ctx.send(ctx.message.content[5::])
+    @commands.command()
+    async def skip(self, ctx):
+        if ctx.guild.voice_client:
+            await ctx.guild.voice_client.disconnect()
+            self.play_next_song(ctx)
+        else:
+            raise commands.CommandError("Not playing any music")  
 
 # #join a voice channel and play music
 # @bot.command()
