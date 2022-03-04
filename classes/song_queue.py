29f43b9afdfedc1220898270bbc8928457268fed
justincdavis <davis.justin@mssm.org>
2022-03-03 21:45:49 -0700

    fixed remove

--- classes/song_queue.py
+++ classes/song_queue.py
@@ -46,7 +46,7 @@ class Song_Queue:
     def clear(self):
         self.songs.clear()
     def remove_by_index(self, index):
-        if(index < 0 or index >= self.get_num_songs()):
+        if(index < 0 or index > self.get_num_songs()):
             return None
         return self.songs.pop(index-1)
 
